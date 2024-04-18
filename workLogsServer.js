f7ea288621ba102a83f8f4bed91903cb781fd6b6
Perry481 <perry20990823@gmail.com>
2024-04-18 15:12:08 +0800

    added the serverlog file

--- workLogsServer.js
+++ workLogsServer.js
@@ -1,31 +1,35 @@
 const http = require("http");
 const { parse } = require("url");
 const next = require("next");
+const fs = require("fs");
 
 const port = parseInt(process.env.PORT, 10) || 3005;
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
+const logFile = "server.log"; // Log file path
+
 app.prepare().then(() => {
   http
     .createServer((req, res) => {
       const parsedUrl = parse(req.url, true);
-
-      // Get current date and time
       const now = new Date();
+      const timestamp = now.toLocaleString();
 
-      // Format date and time
-      const timestamp = now.toLocaleString(); // Adjust format as needed
-
-      // Conditional logging based on URL and method
       if (
         (req.url === "/" && req.method === "GET") ||
         (req.url === "/api/updateDB" && req.method === "POST") ||
         (req.url === "/api/deleteDB" && req.method === "DELETE")
       ) {
-        console.log(
-          `[${timestamp}] Received ${req.method} request from ${req.connection.remoteAddress} for ${req.url}`
-        );
+        const logEntry = `[${timestamp}] Received ${req.method} request from ${req.connection.remoteAddress} for ${req.url}\n`;
+
+        // Write to log file
+        fs.appendFile(logFile, logEntry, (err) => {
+          if (err) console.error("Error writing to log file:", err);
+        });
+
+        // Also log to console
+        console.log(logEntry);
       }
 
       handle(req, res, parsedUrl);
